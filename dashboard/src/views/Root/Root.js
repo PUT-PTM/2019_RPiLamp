3b13fc800e7de642940882300e3ba46197f9fbd4
Sebastian Siejek <kontakt@sebastiansiejek.pl>
2019-05-18 18:49:15 +0200

    add Animation Select component

--- dashboard/src/views/Root/Root.js
+++ dashboard/src/views/Root/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TurnLight from '../components/TurnLight/TurnLight';
+import AnimationSelect from '../components/AnimationSelect/AnimationSelect';
 
 function App() {
   return (
     <div className="App">
       <TurnLight />
+      <AnimationSelect />
     </div>
   );
 }
---
+++ dashboard/src/views/components/AnimationSelect/AnimationSelect.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+export default class TurnLight extends Component {
+  state = {
+    selectedAnimation: 'animation1',
+  };
+
+  handleChange = event => {
+    this.setState({ selectedAnimation: event.target.value }, this.sendState());
+  };
+
+  sendState = () => {
+    // const { selectedAnimation } = this.state;
+    // return fetch(`set?switch-light=${selectedAnimation}`);
+  };
+
+  render() {
+    const { selectedAnimation } = this.state;
+
+    return (
+      <>
+        <FormControl>
+          <InputLabel htmlFor="animation">Animation</InputLabel>
+          <Select
+            value={selectedAnimation}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'animation',
+              id: 'animation',
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="animation1">Animation 1</MenuItem>
+            <MenuItem value="animation2">Animation 2</MenuItem>
+            <MenuItem value="animation3">Animation 3</MenuItem>
+          </Select>
+        </FormControl>
+      </>
+    );
+  }
+}
